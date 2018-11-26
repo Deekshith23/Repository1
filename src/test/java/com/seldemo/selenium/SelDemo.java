f42609e0977858d5a3e2866ef2eb31df958c068b
Deekshith23 <42463817+Deekshith23@users.noreply.github.com>
2018-11-27 03:07:56 +0530

    Commit 2

---
+++ src/test/java/com/seldemo/selenium/SelDemo.java
@@ -0,0 +1,14 @@
+package com.seldemo.selenium;
+
+import org.testng.annotations.Test;
+
+public class SelDemo 
+
+{
+	@Test
+	public void PrintSel()
+	{
+		System.out.println("This is a maven demo project");
+		
+	}
+}
